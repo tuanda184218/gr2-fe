1b0887dade5d91ae74da224db25b3768082998ec
tuanda184218 <tuanpeterkill@gmail.com>
2023-02-23 02:35:33 +0700

    new feature login signup

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,20 +1,30 @@
 import { useState } from "react";
-import "./Login.scss";
 import { Link, useNavigate } from "react-router-dom";
 import { postLogin } from "../../services/apiService";
 import { useDispatch } from "react-redux";
 import { doLogin } from "../../redux/action/userAction";
-import {ImSpinner10} from "react-icons/im";
 import { toast } from "react-toastify";
+import Avatar from '@mui/material/Avatar'
+import Button from '@mui/material/Button'
+import CssBaseline from '@mui/material/CssBaseline'
+import TextField from '@mui/material/TextField'
+import Grid from '@mui/material/Grid'
+import Box from '@mui/material/Box'
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
+import Typography from '@mui/material/Typography'
+import Container from '@mui/material/Container'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
 
 const Login = (props) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const handleLogin = async () => {
+  const theme = createTheme()
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
     //validate
     if (!username) {
       toast.warn("Username can not be blank!");
@@ -26,7 +36,6 @@ const Login = (props) => {
       return;
     }
 
-    setIsLoading(true);
 
     //submit api
 
@@ -37,63 +46,88 @@ const Login = (props) => {
       if (res.data && res.status === 200) {
         localStorage.setItem("user", JSON.stringify(res.data));
         toast.success("Login successfully");
-        setIsLoading(false);
         setTimeout(function () {
           return navigate("/");
         }, 2000);
       }
     } catch (err) {
-      setIsLoading(false);
       toast.error("Username or password is not correct!");
     }
   };
 
   const handleKeyDown = (event) => {
     if(event && event.key === "Enter"){
-      handleLogin();
+        handleSubmit();
     }
   }
 
   return (
-    <div className="signin-container">
-      <div className="header col-4 mx-auto">LOGIN</div>
-      <div className="content-form col-4 mx-auto">
-        <div className="form-group">
-          <label>Username</label>
-          <input
-            type="username"
-            className="form-control"
-            placeholder="Enter your username"
-            value={username}
-            onChange={(event) => setUsername(event?.target?.value)}
-          />
-        </div>
-
-        <div className="form-group">
-          <label>Password</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Enter your password"
-            value={password}
-            onChange={(event) => setPassword(event?.target?.value)}
-            onKeyDown={(event)=>handleKeyDown(event)}
-          />
-        </div>
-
-        <button type="submit" className="button" onClick={() => handleLogin()} disabled={isLoading}>
-          {isLoading === true && <ImSpinner10 className="loader-icon"/>}
-          <span>Login</span>
-        </button>
-        <p className="forgot-password">
-          <Link to={"/reset-pass"}>Password forgot?</Link>
-        </p>
-        <p className="register">
-          Account not existed?
-          <Link to={"/signup"}>Sign up</Link>
-        </p>
-      </div>
-    </div>
+    <ThemeProvider theme={theme}>
+			<Container component="main" maxWidth="xs">
+				<CssBaseline />
+				<Box
+					sx={{
+						marginTop: 8,
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center'
+					}}>
+					<Avatar sx={{ m: 1, bgcolor: '#3f135c' }}>
+						<LockOutlinedIcon />
+					</Avatar>
+					<Typography component="h1" variant="h5" fontWeight={700}>
+						Đăng nhập
+					</Typography>
+					<Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+						<TextField
+							margin="normal"
+							required
+							fullWidth
+							id="username"
+							label="username"
+							name="username"
+							autoComplete="username"
+                            value={username}
+							autoFocus
+                            onChange={(event) => setUsername(event?.target?.value)}
+						/>
+						<TextField
+							margin="normal"
+							required
+							fullWidth
+							name="password"
+							label="password"
+                            value={password}
+							type="password"
+							id="password"
+							autoComplete="current-password"
+                            onChange={(event) => setPassword(event?.target?.value)}
+						/>
+						<Button
+							type="submit"
+							fullWidth
+							variant="contained"
+							style={{ backgroundColor: 'rgba(131, 58, 180, 1)' }}
+							sx={{ mt: 3, mb: 2 }}>
+							Login
+						</Button>
+						<Grid container>
+							<Grid item xs>
+								<Link to={'/reset-pass'} variant="body2">
+                                    Password forgot?
+								</Link>
+							</Grid>
+							<Grid item>
+                                Account not existed?
+								<Link to={'/signup'} variant="body2">
+									{'Sign up'}
+								</Link>
+							</Grid>
+						</Grid>
+					</Box>
+				</Box>
+			</Container>
+		</ThemeProvider>
   );
 };
 
--- src/components/Auth/Signup.js
+++ src/components/Auth/Signup.js
@@ -1,15 +1,28 @@
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import "./Signup.scss";
 import { postRegister } from '../../services/apiService'
 import { toast } from "react-toastify";
+import Avatar from '@mui/material/Avatar'
+import Button from '@mui/material/Button'
+import CssBaseline from '@mui/material/CssBaseline'
+import TextField from '@mui/material/TextField'
+import { Link, useNavigate } from 'react-router-dom'
+import Grid from '@mui/material/Grid'
+import Box from '@mui/material/Box'
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
+import Typography from '@mui/material/Typography'
+import Container from '@mui/material/Container'
+import {createTheme, ThemeProvider } from '@mui/material/styles'
 
-const Signup = (props) => {
+
+
+export default function Signup(){
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [role, setRole] = useState(["ROLE_USER"]);
   const navigate = useNavigate();
+
+  const theme = createTheme()
 
   const validateEmail = (email) => {
     return String(email)
@@ -19,7 +32,8 @@ const Signup = (props) => {
       );
   };
 
-  const handleSignup = async () => {
+  const handleSubmit = async (event) => {
+    event.preventDefault()
     //validate
     if (!username) {
       toast.warn("Username can not be blanked!");
@@ -54,51 +68,72 @@ const Signup = (props) => {
   };
 
   return (
-    <div className="signup-container">
-      <div className="header col-4 mx-auto">Register</div>
-      <div className="content-form col-4 mx-auto">
-        <div className="form-group">
-          <label>Username</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Enter your username"
-            value={username}
-            onChange={(event) => setUsername(event?.target?.value)}
-          />
-        </div>
-        <div className="form-group">
-          <label>Email</label>
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Enter your email"
-            value={email}
-            onChange={(event) => setEmail(event?.target?.value)}
-          />
-        </div>
+    <ThemeProvider theme={theme}>
+			<Container component="main" maxWidth="xs">
+				<CssBaseline />
+				<Box
+					sx={{
+						marginTop: 8,
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center'
+					}}>
+					<Avatar sx={{ m: 1, bgcolor: '#3f135c' }}>
+						<LockOutlinedIcon />
+					</Avatar>
+					<Typography component="h1" variant="h5">
+						Đăng ký
+					</Typography>
+					<Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+						<Grid container spacing={2}>
+							<Grid item xs={12}>
+								<TextField
+									autoComplete="username"
+									name="username"
+									required
+									fullWidth
+									id="username"
+									label="username"
+									autoFocus
+                                    onChange={(event) => setUsername(event?.target?.value)}
+								/>
+							</Grid>
+							<Grid item xs={12}>
+								<TextField required fullWidth id="email" label="email" name="email" autoComplete="email" onChange={(event) => setEmail(event?.target?.value)}/>
+							</Grid>
+							<Grid item xs={12}>
+								<TextField
+									required
+									fullWidth
+									name="password"
+									label="password"
+									type="password"
+									id="password"
+									autoComplete="new-password"
+                                    onChange={(event) => setPassword(event?.target?.value)}
+								/>
+							</Grid>
 
-        <div className="form-group">
-          <label>Password</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="*******"
-            value={password}
-            onChange={(event) => setPassword(event?.target?.value)}
-          />
-        </div>
-
-        <button type="submit" className="button" onClick={() => handleSignup()}>
-          Register
-        </button>
-        <p className="forgot-password">
-          Already registered <Link to={"/login"}>sign in?</Link>
-        </p>
-      </div>
-      {/* <ToastContainer /> */}
-    </div>
+						</Grid>
+						<Button
+							type="submit"
+							fullWidth
+							variant="contained"
+							sx={{ mt: 3, mb: 2 }}
+							style={{ backgroundColor: 'rgba(131, 58, 180, 1)' }}>
+                                Sign up
+						</Button>
+						<Grid container justifyContent="flex-end">
+							<Grid item>
+								<Link to={'/login'} variant="body2">
+									Sign in
+								</Link>
+							</Grid>
+						</Grid>
+					</Box>
+				</Box>
+			</Container>
+		</ThemeProvider>
   );
 };
 
-export default Signup;
